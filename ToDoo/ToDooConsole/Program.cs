bb34c0c8201c8bfbe8bcb7e384a264dac2daa4a8
Hansi Blomberg <hansiblomberg@msn.com>
2015-12-22 01:28:13 +0100

    Gjorde program.cs mer DRY, flyttade upprepad output till metoder.

--- ToDoo/ToDooConsole/Program.cs
+++ ToDoo/ToDooConsole/Program.cs
@@ -103,36 +103,16 @@ namespace ToDoConsole
             // The method is returning a list of ToDo objects.
             List<ToDo> aToDo = channel.GetToDo(name);
 
-            // Now we just have to output some text to the console so that it looks like we have
-            // been busy doing a lot of stuff...
-            Console.WriteLine($"   Output for todolist {name}:");
-
             // Print out some information/properties from the todo item
-            foreach (var todoItem in aToDo)
-            {
-                Console.WriteLine($"ID: {todoItem.Id}");
-                Console.WriteLine($"Beskrivning: {todoItem.Description}");
-                Console.WriteLine($"Skapad datum: {todoItem.CreatedDate}");
-                Console.WriteLine($"Deadline: {todoItem.DeadLine}");
-                Console.WriteLine($"Uppskattad tid att utföra: {todoItem.EstimationTime}");
-                Console.WriteLine($"Färdig? {todoItem.Finnished}");
-                Console.WriteLine("");
-            };
+            ViewToDoItems(aToDo, "todolist", name);
 
-
-            // This is instructions to the user of our console application that the information
-            // can also be retrieved with an internet browser.
-            Console.WriteLine("");
-            Console.WriteLine("This can also be accomplished by navigating to");
-            Console.WriteLine($"http://localhost:8000/GetToDo/{name}");
-            Console.WriteLine("in a web browser while this sample is running.");
-
-            Console.WriteLine("");
-
+            // Display how to access them via uri
+            ViewWebInstructions($"/GetToDo/{name}");
 
         }
 
 
+        
 
         // Test the GetDone WCF method
         static void DemoGetDone(IToDoService channel, string name)
@@ -195,32 +175,12 @@ namespace ToDoConsole
             // The method is returning a list of ToDo objects.
             List<ToDo> aToDo = channel.GetDone(name);
 
-            // Now we just have to output some text to the console so that it looks like we have
-            // been busy doing a lot of stuff...
-            Console.WriteLine($"   Output for donelist {name}:");
-
             // Print out some information/properties from the todo item
-            foreach (var todoItem in aToDo)
-            {
-                Console.WriteLine($"ID: {todoItem.Id}");
-                Console.WriteLine($"Beskrivning: {todoItem.Description}");
-                Console.WriteLine($"Skapad datum: {todoItem.CreatedDate}");
-                Console.WriteLine($"Deadline: {todoItem.DeadLine}");
-                Console.WriteLine($"Uppskattad tid att utföra: {todoItem.EstimationTime}");
-                Console.WriteLine($"Färdig? {todoItem.Finnished}");
-                Console.WriteLine("");
-            };
-
+            ViewToDoItems(aToDo, "donelist", name);
 
             // This is instructions to the user of our console application that the information
             // can also be retrieved with an internet browser.
-            Console.WriteLine("");
-            Console.WriteLine("This can also be accomplished by navigating to");
-            Console.WriteLine($"http://localhost:8000/GetDone/{name}");
-            Console.WriteLine("in a web browser while this sample is running.");
-
-            Console.WriteLine("");
-
+            ViewWebInstructions($"/GetDone/{name}");
 
         }
 
@@ -356,6 +316,41 @@ namespace ToDoConsole
 
 
         }
+
+
+        // Shows instructions to the user of our console application that the information
+        // can also be retrieved with an internet browser.
+        static void ViewWebInstructions(string urlPart)
+        {
+
+            Console.WriteLine("");
+            Console.WriteLine("This can also be accomplished by navigating to");
+            Console.WriteLine($"http://localhost:8000{urlPart}");
+            Console.WriteLine("in a web browser while this sample is running.");
+            Console.WriteLine();
+
+        }
+
+
+        // Prints out some information/properties from the todo items
+        static void ViewToDoItems(List<ToDo> aToDo, string demoMethodName, string name)
+        {
+            Console.WriteLine($"   Output for {demoMethodName} {name}:");
+
+            // Print out some information/properties from the todo item
+            foreach (var todoItem in aToDo)
+            {
+                Console.WriteLine($"ID: {todoItem.Id}");
+                Console.WriteLine($"Beskrivning: {todoItem.Description}");
+                Console.WriteLine($"Skapad datum: {todoItem.CreatedDate}");
+                Console.WriteLine($"Deadline: {todoItem.DeadLine}");
+                Console.WriteLine($"Uppskattad tid att utföra: {todoItem.EstimationTime}");
+                Console.WriteLine($"Färdig? {todoItem.Finnished}");
+                Console.WriteLine("");
+            };
+
+        }
+
 
     }
 
